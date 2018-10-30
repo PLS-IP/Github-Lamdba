80db2cc520b2cdafa862f6836539df1b9aaf3245
Liron Kopinsky <liron.kopinsky@plsip.com>
2018-10-30 13:43:46 +0200

    Testing modifications and build system.
    Testing Description

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ var path = require('path');
 
 exports.get = function(event, context, callback) {
   var contents = fs.readFileSync(`public${path.sep}index.html`);
+  var body = contents.toString();
+  body = body
+    .replace("Seattle.", "Seattle. Modified in Jerusalem.")
+    .replace("Seattle", "Washington State")
   var result = {
     statusCode: 200,
-    body: contents.toString(),
+    body: body,
     headers: {'content-type': 'text/html'}
   };
 
